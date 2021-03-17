88bc61a88640ec5e59c5fd8b584d5811a3881d0a
Roshan Sharma <reprogram46@gmail.com>
2021-03-18 04:27:22 +0530

    wip: Return JWT on login

--- app/routes/auth/login.py
+++ app/routes/auth/login.py
@@ -1,15 +1,28 @@
+from datetime import datetime, timedelta
+
+import jwt
 from app import app
 from fastapi import Request
+from fastapi.responses import JSONResponse
 
 USERNAME = "rosh"
 PASSWORD = "rosh"
+SECRET_KEY = "loadthisfromenv"
 
 
 @app.post("/login")
 async def login(request: Request):
     body = await request.json()
+    payload = {
+        "exp": datetime.utcnow() + timedelta(days=0, hours=24),
+        "iat": datetime.utcnow(),
+    }
+    token = jwt.encode(payload, SECRET_KEY, algorithm="HS256").decode("utf-8")
     u = body["username"]
     p = body["password"]
     if u == USERNAME and p == PASSWORD:
-        return {"token": "58g84yckr8tbuwsk04j"}
-    return {}
+        content = {"result": "ok"}
+        response = JSONResponse(content=content)
+        response.headers["X-AUTH-TOKEN"] = f"{(token)}"
+        return response
+    return {"result": "fail"}
