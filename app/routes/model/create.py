7905fb4ae5888c260827e12cfdceb5a34335ce12
Roshan Sharma <reprogram46@gmail.com>
2021-03-24 16:19:00 +0530

    create.py: Parse model details from request body
    Generate uuid & pass to client to send in next request as filename.

--- app/routes/model/create.py
+++ app/routes/model/create.py
@@ -1,5 +1,6 @@
 import json
 import os
+import uuid
 
 import jwt
 from app import app
@@ -9,45 +10,36 @@ from app.database.models import DeployedModel, User
 from fastapi import File, Request, UploadFile, status
 from starlette.responses import Response
 
-STORAGE_DIR = os.path.join(os.getcwd(), "storage")
-
 
 @app.post("/model/create")
-async def create_model(req: Request, resp: Response, model: UploadFile = File(...)):
+async def create_model(req: Request, resp: Response):
     if "X-Auth-Token".lower() not in req.headers.keys():
         resp.status_code = status.HTTP_401_UNAUTHORIZED
         return {"result": "fail", "reason": "No auth token specified"}
 
+    # TODO: Check header key case
     token = req.headers["X-Auth-Token"]
     try:
-        decoded = json.loads(
+        _ = json.loads(
             json.dumps(jwt.decode(token, ACCESS_TOKEN_KEY, algorithms="HS256"))
         )
     except Exception as e:
         resp.status_code = status.HTTP_401_UNAUTHORIZED
         return {"result": "fail", "reason": str(e)}
 
-    if "X-Model-Author".lower() not in req.headers.keys():
-        resp.status_code = status.HTTP_400_BAD_REQUEST
-        return {"result": "fail", "reason": "No author specified"}
+    # TODO: Use pydantic models to auto-validate body params
+    body = await req.json()
+    name = body["name"]
+    username = body["username"]
+    description = body["description"]
 
-    # TODO: Check header key case
-    username = req.headers["X-Model-Author"]
-
-    if not os.path.exists(STORAGE_DIR):
-        os.mkdir(STORAGE_DIR)
-
-    filename = f"{username}-{model.filename}"
-    try:
-        with open(os.path.join(STORAGE_DIR, filename), "wb") as fp:
-            fp.write(model.file.read())
-    except Exception as e:
-        resp.status_code = status.HTTP_507_INSUFFICIENT_STORAGE
-        return {"result": "fail", "reason": str(e)}
-
+    uid_slug = uuid.uuid4().hex[:6]
+    model_uid = f"{username}-{uid_slug}"
     try:
         user = db.query(User).filter_by(username=username).first()
-        m = DeployedModel(name=filename, author=user, description="OK")
+        m = DeployedModel(
+            name=name, uid=model_uid, author=user, description=description
+        )
         db.add(m)
         db.commit()
     except Exception as e:
@@ -55,4 +47,4 @@ async def create_model(req: Request, resp: Response, model: UploadFile = File(..
         resp.status_code = status.HTTP_400_BAD_REQUEST
         return {"result": "fail", "reason": str(e)}
 
-    return {"result": "ok", "filename": filename}
+    return {"result": "ok", "uid": model_uid}
