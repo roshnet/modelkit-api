5a79200a13a07261fb98d23d8f6ae8e1d2261c9e
Roshan Sharma <reprogram46@gmail.com>
2021-04-15 02:28:12 +0530

    fix: Modify response structure when fetching a model

--- app/routes/model/fetch_one.py
+++ app/routes/model/fetch_one.py
@@ -10,9 +10,12 @@ async def fetch_one(uid: str, resp: Response):
     # TODO: Choose between .first() and .one() to fetch a model.
     # Using .one() throws an exception for zero or >1 results, hence using .first().
     model = (
-        db.query(DeployedModel, User.username).filter(DeployedModel.uid == uid).first()
+        db.query(DeployedModel.name, DeployedModel.description, User.username).filter(
+            DeployedModel.uid == uid
+        ).first()
     )
     if not model:
         resp.status_code = status.HTTP_404_NOT_FOUND
         return {"result": "fail", "reason": "No model found for given UID"}
+
     return {"result": "ok", "model": model}
