eefabebe0ce3d4505cf3f16533c363da2bf1a15e
Roshan Sharma <reprogram46@gmail.com>
2021-04-16 20:58:15 +0530

    feed.py: Pass params as query instead of body

--- app/routes/feed.py
+++ app/routes/feed.py
@@ -1,19 +1,10 @@
-from typing import Optional
-
 from app import app
-from app.database import db, models
+from app.database import db
 from app.database.models import DeployedModel as Model
 from app.database.models import User
-from pydantic import BaseModel
-
-
-class RequestBody(BaseModel):
-    start: Optional[int] = 0
-    count: Optional[int] = 10
 
 
 @app.get("/feed")
-async def feed(req: RequestBody):
-    models = db.query(Model).join(User).offset(req.start).limit(req.count).all()
-
+async def feed(start=0, count=10):
+    models = db.query(Model).join(User).offset(start).limit(count).all()
     return {"status": "ok", "models": models}
