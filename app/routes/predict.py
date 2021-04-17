6bfbcfc4e0cfddc9b9a2f5667063822015a757ad
Roshan Sharma <reprogram46@gmail.com>
2021-04-17 22:25:07 +0530

    predict.py: Add suitable status code to possible responses

--- app/routes/predict.py
+++ app/routes/predict.py
@@ -4,6 +4,8 @@ from typing import List
 import joblib
 from app import app
 from pydantic import BaseModel
+from starlette import status
+from starlette.responses import Response
 
 STORAGE_DIR = os.path.join(os.getcwd(), "storage")
 
@@ -14,12 +16,13 @@ class RequestBody(BaseModel):
 
 
 @app.post("/predict")
-async def train(req: RequestBody):
+async def train(req: RequestBody, resp: Response):
     model_file = os.path.join(STORAGE_DIR, req.model_uid)
 
     try:
         clf = joblib.load(open(model_file, "rb"))
     except FileNotFoundError:
+        resp.status_code = status.HTTP_404_NOT_FOUND
         return {"status": "fail", "reason": "Model binary not found"}
     if not hasattr(clf, "predict"):
         return {"status": "ok"}
@@ -27,6 +30,7 @@ async def train(req: RequestBody):
     try:
         prediction = clf.predict(req.xtest)
     except Exception as e:
+        resp.status_code = status.HTTP_417_EXPECTATION_FAILED
         return {"status": "fail", "reason": str(e)}
 
     return {"status": "ok", "prediction": str(prediction)}
