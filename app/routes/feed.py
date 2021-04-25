507766d6aaeba9d8382a3b7ecc6c603208af5966
Roshan Sharma <reprogram46@gmail.com>
2021-04-25 16:21:00 +0530

    feed.py: Return required fields after join

--- app/routes/feed.py
+++ app/routes/feed.py
@@ -6,5 +6,11 @@ from app.database.models import User
 
 @app.get("/feed")
 async def feed(start=0, count=10):
-    models = db.query(Model).join(User).offset(start).limit(count).all()
+    models = (
+        db.query(Model.name, Model.description, Model.uid, User.username)
+        .join(User)
+        .offset(start)
+        .limit(count)
+        .all()
+    )
     return {"status": "ok", "models": models}
